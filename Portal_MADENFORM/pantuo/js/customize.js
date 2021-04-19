6b982123e194f902c41e6b7c0bb6293dd61bae95
yutingh <yutingh@cw.com.tw>
2021-04-19 23:00:29 +0800

    rwd

--- Portal_MADENFORM/pantuo/js/customize.js
+++ Portal_MADENFORM/pantuo/js/customize.js
@@ -1,18 +1,15 @@
 $(document).ready(function () {
+    var orderIdx = ['A1 ~ A6', 'A7', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'B11', 'B12', 'B13', 'B14', 'B15', 'B16', 'B17', 'C1', 'C2', 'C3', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'E1', 'E2', 'E3', 'E4'],
+        orderIdxLength = orderIdx.length;
+    console.log(orderIdxLength);
     // 步驟樣式
     var txt = $('.list-order').text();
-    if ( txt.indexOf('A') >= 0 ) {
-        $('.section-contents h2 span, .section-contents-one h2 span').addClass('lv1');
-    } else if ( txt.indexOf('B') >= 0 ) {
-        $('.section-contents h2 span, .section-contents-one h2 span').addClass('lv2');
-    } else if ( txt.indexOf('C') >= 0 ) {
-        $('.section-contents h2 span, .section-contents-one h2 span').addClass('lv3');
-    } else if ( txt.indexOf('D') >= 0 ) {
-        $('.section-contents h2 span, .section-contents-one h2 span').addClass('lv4');
-    } else if ( txt.indexOf('E') >= 0 ) {
-        $('.section-contents h2 span, .section-contents-one h2 span').addClass('lv5');
+    for (var i = 0; i < orderIdxLength; i++) {
+        if ( txt.indexOf(orderIdx[i]) >= 0 ) {
+            console.log(orderIdx[i]);
+            $('.section-contents h2 span, .section-contents-one h2 span').css('width', ((i+1)/orderIdxLength)*100 + '%');
+        }
     }
-
     // contents 最小高度
     var height = $(Window).height(),
         headerHeight = $('header').outerHeight(),
